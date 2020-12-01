82cbbf789c8da73b05b12fdd6e9d2572a71bb271
Seraj Dhaliwal <serajdhaliwal@gmail.com>
2020-11-30 21:37:00 -0500

    initial re-do; just map with some out-of-box controls

--- js/app.js
+++ js/app.js
@@ -28,28 +28,15 @@ require([
   "esri/dijit/Popup",
   "esri/symbols/SimpleFillSymbol",
   "esri/Color",
-  "digits/overlayManager/js/overlayManager",
-  "digits/about/js/about",
-  "digits/batchSelect/js/batchSelect",
-  "digits/legend/js/legend",
-  "digits/quickIcon/js/quickicon",
-  "esri/dijit/BasemapLayer",
-  "esri/dijit/Basemap",
-  "digits/basemapGallery/js/basemapGallery",
-  "esri/layers/ArcGISTiledMapServiceLayer",
   "esri/dijit/Scalebar",
   "esri/dijit/Measurement",
   "esri/dijit/Bookmarks",
   "esri/geometry/Extent",
   "dojo/parser",
-  "digits/featureMenu/js/featureMenu",
   "dojo/dom-class",
   "dojo/dom-construct",
   "esri/units",
-  "esri/tasks/locator",
-  "app/extensions/esriMySearch",
   "esri/geometry/projection",
-  "app/JSUtils",
   "notify/notify.min",
   "dojo/domReady!"
 ], function (
@@ -57,28 +44,15 @@ require([
   Popup,
   SimpleFillSymbol,
   Color,
-  OverlayManager,
-  About,
-  BatchSelect,
-  Legend,
-  QuickIcon,
-  BasemapLayer,
-  Basemap,
-  BasemapGallery,
-  ArcGISTiledMapServiceLayer,
   Scalebar,
   Measurement,
   Bookmarks,
   Extent,
   parser,
-  featureMenu,
-  domClass,
+  ddomClass,
   domConstruct,
   Units,
-  Locator,
-  EsriMySearch,
-  projection,
-  JSUtils
+  projection
 ) {
   var fill = new SimpleFillSymbol("solid", null, new Color("#A4CE67"));
   var popup = new Popup({
@@ -89,7 +63,7 @@ require([
   );
 
   var map = new Map("map", {
-    //  basemap: "streets",
+    basemap: "streets",
     extent: new Extent({
       "xmin": -16045622,
       "ymin": -811556,
@@ -129,294 +103,6 @@ require([
 
   parser.parse();
 
-  var featuremenu = new featureMenu({
-    map: map,
-    title: "Add Point Features to the Map",
-    targetNodeIds: ["map"]
-  }).initialize();
-
-  var legend = new Legend(map);
-
-  /** Create a esriMySearch object */
-  var esriSearch = new EsriMySearch();
-
-  /** Extend the search class with esriSearch's initSearch() */
-  var MySearch = esriSearch.initSearch();
-  var search = new MySearch({
-    map: map,
-    enableButtonMode: false,
-    enableLabel: false,
-    enableSearchingAll: false,
-    enableInfoWindow: true,
-    enableInfoWindowOnSelect: true,
-    // addLayersFromMap: true,
-    // enableSearchingAll: true,
-    enableSourcesMenu: true,
-    zoomScale: 100000
-  },
-    "search"
-  );
-
-  /** Subscribing the search object to the ActiveSourceIndex channel */
-  /*
-  OWF.Eventing.subscribe("com.esri.owf.map.activeSourceIndex", function (
-    sender,
-    msg
-  ) {
-    msg = parseInt(msg);
-    if ((sender.id === OWF.getInstanceId()) || isNaN(msg)) {
-      return;
-    } else {
-      search.set("activeSourceIndex", msg);
-    }
-  });
-  */
-  var sources = search.get("sources");
-
-  sources.push({
-    locator: new Locator(
-      "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/"
-    ),
-    // highlightSymbol: {url: "https://js.arcgis.com/3.26/esri/dijit/Search/images/search-pointer.png", width: 36, height: 36, xoffset: 9, yoffset: 18},
-    singleLineFieldName: "SingleLine",
-    localSearchOptions: {
-      minScale: 300000,
-      distance: 50000
-    },
-    outFields: ["Addr_type", "Match_addr", "StAddr", "City"],
-    name: "Lat/Long D.D",
-    placeholder: "(+/-)DD.D(N/S), (+/-)DDD.D(E/W)",
-    enableSuggestions: false
-  });
-
-  sources.push({
-    locator: new Locator(
-      "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/"
-    ),
-    singleLineFieldName: "SingleLine",
-    localSearchOptions: {
-      minScale: 300000,
-      distance: 50000
-    },
-    outFields: ["Addr_type", "Match_addr", "StAddr", "City"],
-    name: "Lat/Long D/M.m",
-    placeholder: "(+/-)DD MM.M(N/S), (+/-)DDD MM.M(E/W)",
-    enableSuggestions: false
-  });
-
-  sources.push({
-    locator: new Locator(
-      "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/"
-    ),
-    singleLineFieldName: "SingleLine",
-    localSearchOptions: {
-      minScale: 300000,
-      distance: 50000
-    },
-    outFields: ["Addr_type", "Match_addr", "StAddr", "City"],
-    name: "Lat/Long D/M/S.s",
-    placeholder: "(+/-)DD MM SS.S(N/S), (+/-)DDD MM SS.S(E/W)",
-    enableSuggestions: false
-  });
-
-  sources.push({
-    locator: new Locator(
-      "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?f=pjson&outFields=Addr_type&maxLocations=1&forStorage=false&SingleLine=18SUH6789043210"
-    ),
-    singleLineFieldName: "SingleLine",
-    localSearchOptions: {
-      minScale: 300000,
-      distance: 50000
-    },
-    outFields: ["Addr_type", "Match_addr", "StAddr", "City"],
-    name: "MGRS / USNG",
-    placeholder: "NO SPACES",
-    enableSuggestions: false
-  });
-
-  /** Set the sources above to the search widget */
-  search.set("sources", sources);
-
-  /** Implement a function to save the source preference of the user */
-  search.savePreferences = function () {
-  };
-
-  /** Implement a function to retrieve the source preference of the user */
-  search.retrievePreferences = function () {
-  };
-
-  /** Add extra functionality to the search-results method */
-  search.on("search-results", function () {
-    /**
-     * Create a conditional when searching for an address within Source 0:
-     *  if the returned search result is not null, color the search box white
-     *  else, color the search box pink
-     */
-    if (search.get("activeSourceIndex") == 0) {
-      if (search.get("searchResults") != null) {
-        esriSearch.changeInputBoxColorOnSuccess();
-      } else {
-        esriSearch.changeInputBoxColorOnError();
-        esriSearch.displayError();
-      }
-    }
-  });
-
-  /** Add extra functionality to the clear-search method */
-  search.on("clear-search", function () {
-    /**
-     * Color the search box white when cleared
-     * If the error box is showing
-     *  then fade it out
-     */
-    esriSearch.changeInputBoxColorOnSuccess();
-  });
-
-  search.on("load", function () {
-    $("#search_input").attr(
-      "placeholder",
-      "Find lat,lon / address / name."
-    );
-
-    /** Call the function to retrieve preferences */
-    search.retrievePreferences();
-  });
-
-  /**
-   * Call the function to save preferences and notify
-   * other maps of the user's search selection.
-   */
-  $("div.searchMenu.sourcesMenu").on('click', function () {
-    /*
-    OWF.Eventing.publish(
-      "com.esri.owf.map.activeSourceIndex",
-      search.get("activeSourceIndex")
-    );
-      */
-    /* save search option */
-    if (search.get("activeSourceIndex") <= 5) {
-      search.savePreferences();
-    }
-
-    closeKMLOverlappingWrapper();
-  });
-
-  var basemapGallery = new BasemapGallery({
-    showArcGISBasemaps: true,
-    /* 20191029 - depreciated due to no license key
-    bingMapsKey: dojoConfig.siteKeys.BingMapsToken,
-    */
-    map: map
-  },
-    "basemapGallery"
-  );
-
-  // added usgs basemaps for backup
-  basemapGallery.add(
-    new Basemap({
-      "layers": [new BasemapLayer({
-        "url": "https://basemap.nationalmap.gov/arcgis/rest/services/USGSHydroCached/MapServer"
-      })],
-      "title": "USGS Hydro Cached",
-      "itemId": "USCG001",
-      "thumbnailUrl": "https://basemap.nationalmap.gov/arcgis/rest/services/USGSHydroCached/MapServer/info/thumbnail"
-    }));
-  basemapGallery.add(
-    new Basemap({
-      "layers": [new BasemapLayer({
-        "url": "https://basemap.nationalmap.gov/arcgis/rest/services/USGSShadedReliefOnly/MapServer"
-      })],
-      "title": "USGS Shaded Relief",
-      "itemId": "USCG002",
-      "thumbnailUrl": "https://basemap.nationalmap.gov/arcgis/rest/services/USGSShadedReliefOnly/MapServer/info/thumbnail"
-    }));
-  basemapGallery.add(
-    new Basemap({
-      "layers": [new BasemapLayer({
-        "url": "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer"
-      })],
-      "title": "USGS ImageryOnly",
-      "itemId": "USCG003",
-      "thumbnailUrl": "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/info/thumbnail"
-    }));
-  basemapGallery.add(
-    new Basemap({
-      "layers": [new BasemapLayer({
-        "url": "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer"
-      })],
-      "title": "USGS Imagery Topo",
-      "itemId": "USCG004",
-      "thumbnailUrl": "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/info/thumbnail"
-    }));
-  basemapGallery.add(
-    new Basemap({
-      "layers": [new BasemapLayer({
-        "url": "https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer"
-      })],
-      "title": "USGS Topo",
-      "itemId": "USCG005",
-      "thumbnailUrl": "https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/info/thumbnail"
-    }));
-  basemapGallery.add(
-    new Basemap({
-      "layers": [new BasemapLayer({
-        "url": "https://10.50.193.71:6443/arcgis/rest/services/InternalBaseMap/MapServer"
-      })],
-      "title": "USCG FailSafe",
-      "itemId": "USCG006",
-      "thumbnailUrl": "/GlobalRepo/Images/Basemaps/basemap.png"
-    }));
-
-  // added bing base maps
-  /* 20191029 - depreciated due to no license key
-  basemapGallery.add(
-    new Basemap({
-      "layers": [new BasemapLayer({ type: "BingMapsRoad" })],
-      "title": "Bing Road",
-      "itemId": "USCG006",
-      "id": "bmRoad",
-      "thumbnailUrl":"images/bing_streets.png"
-    }));
-    basemapGallery.add(
-      new Basemap({
-        "layers": [new BasemapLayer({ type: "BingMapsAerial" })],
-        "title": "Bing Aerial",
-        "itemId": "USCG007",
-        "id": "bmAerial",
-        "thumbnailUrl":"images/bing_aerial.png"
-      }));
-      basemapGallery.add(
-        new Basemap({
-          "layers": [new BasemapLayer({ type: "BingMapsHybrid" })],
-          "title": "Bing Aerial with labels",
-          "itemId": "USCG008",
-          "id": "bmHybrid",
-          "thumbnailUrl":"images/bing_aerial_hybrid.png"
-        }));
-  */
-  // added site verification
-  JSUtils.isSiteOnline("https://www.arcgis.com", function (found) {
-    if (found) {
-      map.setBasemap("streets");
-    } else {
-      window.alert("Temporary ArcGIS service outage; alternate basemap set to USGS Topo.\n\n** This impacts REFERENCE/WEATHER services from ArcGIS.com; NOAA and other sites are not impacted. **\n\nIf no basemaps are available due to network outage; please use one of the three alternate options: (a) USCG Failsafe basemap, (b) USGS basemaps, or (c) Catalog Widget World Countries KML (REFERENCE -> BOUNDARIES).\n\nSee Alert Feed Widget for more details.");
-      var tBasemaps = basemapGallery.basemaps;
-      $.each(tBasemaps, function (index, item) {
-        if (item.title === "USGS Topo") {
-          var currBasemap = map.getLayer('basemap');
-          if (currBasemap) {
-            map.removeLayer(currBasemap);
-          }
-
-          var newBasemap = new ArcGISTiledMapServiceLayer(item.layers[0].url, {
-            "id": "basemap"
-          });
-          map.addLayer(newBasemap, 0);
-        }
-      });
-    }
-  });
-
   new Scalebar({
     map: map,
     attachTo: "bottom-left",
@@ -441,7 +127,7 @@ require([
 
   var toggleBasemapGallery = function () {
     $(
-      "#overlay_wrapper, #about_wrapper, #batchselect_wrapper, #draw_wrapper, #edit_wrapper, #bookmark_wrapper, #measure_wrapper, #quickIcon_wrapper"
+      "#overlay_wrapper, #about_wrapper, #batchselect_wrapper, #legend_wrapper, , #draw_wrapper, #edit_wrapper, #bookmark_wrapper, #measure_wrapper, #quickIcon_wrapper"
     ).hide();
     $(
       "#overlay, #about, #batchselect, #legend, #draw, #bookmark, #measure, #edit, #quickIcon"
@@ -455,7 +141,7 @@ require([
 
   var toggleDraw = function () {
     $(
-      "#overlay_wrapper, #about_wrapper, #batchselect_wrapper, #basemaps_wrapper, #edit_wrapper, #bookmark_wrapper, #measure_wrapper, #quickIcon_wrapper"
+      "#overlay_wrapper, #about_wrapper, #batchselect_wrapper, #legend_wrapper, , #basemaps_wrapper, #edit_wrapper, #bookmark_wrapper, #measure_wrapper, #quickIcon_wrapper"
     ).hide();
     $(
       "#basemaps, #overlay, #about, #batchselect, #legend, #bookmark, #measure, #edit, #quickIcon"
@@ -469,7 +155,7 @@ require([
 
   var toggleMeasure = function () {
     $(
-      "#overlay_wrapper, #about_wrapper, #batchselect_wrapper, #basemaps_wrapper, #draw_wrapper, #edit_wrapper, #bookmark_wrapper, #quickIcon_wrapper"
+      "#overlay_wrapper, #about_wrapper, #batchselect_wrapper, #legend_wrapper, , #basemaps_wrapper, #draw_wrapper, #edit_wrapper, #bookmark_wrapper, #quickIcon_wrapper"
     ).hide();
     $(
       "#basemaps, #overlay, #about, #batchselect, #legend, #draw, #bookmark, #edit, #quickIcon"
@@ -483,7 +169,7 @@ require([
 
   var toggleBookmark = function () {
     $(
-      "#overlay_wrapper, #about_wrapper, #batchselect_wrapper, #basemaps_wrapper, #draw_wrapper, #edit_wrapper, #measure_wrapper, #quickIcon_wrapper"
+      "#overlay_wrapper, #about_wrapper, #batchselect_wrapper, #legend_wrapper, , #basemaps_wrapper, #draw_wrapper, #edit_wrapper, #measure_wrapper, #quickIcon_wrapper"
     ).hide();
     $(
       "#basemaps, #overlay, #about, #batchselect, #legend, #draw, #measure, #edit, #quickIcon"
@@ -497,7 +183,7 @@ require([
 
   var toggleQuickIcon = function () {
     $(
-      "#overlay_wrapper, #about_wrapper, #batchselect_wrapper, #basemaps_wrapper, #edit_wrapper, #bookmark_wrapper, #measure_wrapper, #draw_wrapper"
+      "#overlay_wrapper, #about_wrapper, #batchselect_wrapper, #legend_wrapper, , #basemaps_wrapper, #edit_wrapper, #bookmark_wrapper, #measure_wrapper, #draw_wrapper"
     ).hide();
     $(
       "#basemaps, #overlay, #about, #batchselect, #legend, #bookmark, #measure, #edit, #draw"
@@ -511,7 +197,7 @@ require([
 
   var toggleControls = function () {
     $(
-      "#overlay_wrapper, #about_wrapper, #batchselect_wrapper, #basemaps_wrapper, #edit_wrapper, #bookmark_wrapper, #measure_wrapper, #draw_wrapper, #quickIcon_wrapper"
+      "#overlay_wrapper, #about_wrapper, #batchselect_wrapper, #legend_wrapper, #basemaps_wrapper, #edit_wrapper, #bookmark_wrapper, #measure_wrapper, #draw_wrapper, #quickIcon_wrapper"
     ).hide();
     $(
       "#basemaps, #overlay, #about, #batchselect, #legend, #bookmark, #measure, #edit, #draw, #quickIcon"
@@ -529,15 +215,6 @@ require([
     } else {
       document.getElementById("sidenav").style.width = "250px";
     }
-  };
-
-  var homeClick = function () {
-    overlayManager.adapter.view.handleCenterBounds(
-      null,
-      overlayManager.adapter.overlayManager.data.utilities.homeExtent
-    );
-
-    closeKMLOverlappingWrapper();
   };
 
   $('#kml_overlaps_close').on("click", function () {
@@ -589,42 +266,12 @@ require([
   // esri.config.defaults.io.proxyUrl = "https://localhost:8443/Java/proxy.jsp";
 
   // moved all startups here for preferences to work correctly
-  featuremenu.startup();
-  search.startup();
-  basemapGallery.startup();
   measurement.startup();
   bookmarks.startup();
 
   // add projection load for global use
   const projectionPromise = projection.load();
 
-  var overlayManager = new OverlayManager({
-    map: map,
-    basemapGallery: basemapGallery,
-    measurement: measurement,
-    bookmarks: bookmarks,
-    search: search,
-    projectionPromise: projectionPromise
-  },
-    errorNotifier,
-    infoNotifier
-  );
-  map.OverlayManager = overlayManager;
-
-  var about = new About({
-    map: map,
-    basemapGallery: basemapGallery
-  },
-    errorNotifier,
-    infoNotifier
-  );
-  var batchselect = new BatchSelect({
-    map: map,
-    basemapGallery: basemapGallery
-  },
-    errorNotifier,
-    infoNotifier
-  );
   // var edit = new Edit({ map: map, basemapGallery: basemapGallery }, errorNotifier, infoNotifier);
   $("#map").on("mouseup", function () {
     $(
@@ -636,32 +283,13 @@ require([
     // $('#data_div_button').removeClass('selected');
   });
 
-  /**
-   * Check and see if the sources menu is open,
-   *  if so, check to see if an error display is open,
-   *   if so, close that display to allow the user to see the sources
-   */
-  $("#search_menu_button").on(
-    "click",
-    esriSearch.changeInputBoxColorOnSuccess
-  );
-
-  $("#overlay").on("click", overlayManager.toggleOverlayManager);
-  $("#about").on("click", about.toggleAbout);
-  $("#batchselect").on("click", batchselect.toggleBatchSelect);
-  // $('#edit').on('click', edit.toggleEdit);
-  $("#basemaps").on("click", toggleBasemapGallery);
-  $("#legend").on("click", legend.handleLegendPopOut);
-  // $('#data_div_button').on('click', handleDataDivPopOut);
   $("[rel=tooltip]").tooltip({
     placement: "bottom"
   });
-  $("#draw").on("click", toggleDraw);
   $("#measure").on("click", toggleMeasure);
   $("#bookmark").on("click", toggleBookmark);
   $("#controls").on("click", toggleSidenav);
-  $("#home").on("click", homeClick);
-  $("#quickIcon").on("click", toggleQuickIcon);
-
   $("#control-slider").on("click", toggleControls);
+  
+  toggleControls();
 });
--- js/extensions/Vendor/README.md
+++
@@ -1,4 +0,0 @@
-# Vendor Specific Files For Use With CMWAPI Adapter
-
-## Description
-The files contained within this directory are third-party modules required to support the adapter code.
