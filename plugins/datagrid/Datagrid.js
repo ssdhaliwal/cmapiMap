88d7d67248bd96bcd0f333ac1e30b9d98f145870
Seraj Dhaliwal <seraj.dhaliwal@gmail.com>
2021-03-30 14:02:56 -0400

    fixed datagrid tab view

--- plugins/datagrid/Datagrid.js
+++ plugins/datagrid/Datagrid.js
@@ -29,7 +29,8 @@ define(["dojo/_base/lang", "dijit/registry", "dojo/query",
                 self.showing = !self.showing;
 
                 if (self.showing) {
-                    $("#datagrid_wrapper").css("display", "block");
+                    //$("#datagrid_wrapper").css("display", "block");
+                    $("#datagrid_wrapper").css("z-index", "99");
 
                     // loop through the grids and resize them
                     Object.keys(self.tabs).forEach(tab => {
@@ -38,7 +39,8 @@ define(["dojo/_base/lang", "dijit/registry", "dojo/query",
                         }
                     });
                 } else {
-                    $("#datagrid_wrapper").css("display", "none");
+                    //$("#datagrid_wrapper").css("display", "none");
+                    $("#datagrid_wrapper").css("z-index", "-1");
                 }
 
                 $("#datagrid").toggleClass("selected");
@@ -56,8 +58,8 @@ define(["dojo/_base/lang", "dijit/registry", "dojo/query",
                 console.log("extDatagrid - addTab");
 
                 // if tab already exists, exit
-                if (self.tabs['grid_' + serviceObject.service.text]) {
-                    return;
+                if (self.tabs['tab_' + serviceObject.service.text]) {
+                    self.removeTab(serviceObject);
                 }
 
                 // auto add if feature or local kml layer type
@@ -67,14 +69,16 @@ define(["dojo/_base/lang", "dijit/registry", "dojo/query",
                     id: 'div_' + serviceObject.service.text,
                     class: "datagridDiv"
                 });
-                self.tabs['div_' + serviceObject.service.text] = gridDiv;
 
                 let cp3 = new ContentPane({
-                    id: 'content_' + serviceObject.service.text,
+                    id: 'tab_' + serviceObject.service.text,
                     title: serviceObject.service.text,
                     content: gridDiv
                 });
-                self.tabs['content_' + serviceObject.service.text] = cp3;
+
+                // add in the sequence (for resize on load)
+                self.tabs['tab_' + serviceObject.service.text] = cp3;
+                self.tabs['div_' + serviceObject.service.text] = gridDiv;
 
                 // to form the grid, we need to get the service object to provide id, structure, and data
                 let checkIW = setInterval(() => {
@@ -83,45 +87,47 @@ define(["dojo/_base/lang", "dijit/registry", "dojo/query",
                         let serviceData = serviceObject.service.perspective.getData();
 
                         serviceData.then(payload => {
-                            console.log(payload, Object.keys(payload.items[0]));
+                            if (payload.items.length > 0) {
+                                console.log(payload, Object.keys(payload.items[0]));
 
-                            // format the data for store/grid
-                            let dataStore = new ItemFileWriteStore({
-                                data: payload
-                            });
-    
-                            // define the grid layout based on keys
-                            let layout = [[]];
-                            Object.keys(payload.items[0]).forEach(key => {
-                                layout[0].push({ 'name': key, 'field': key, 'width': '100px' });
-                            });
-    
-                            let grid = new DataGrid({
-                                id: 'grid_' + serviceObject.service.text,
-                                store: dataStore,
-                                structure: layout,
-                                rowSelector: '20px'
-                            });
-    
-                            grid.on("RowClick", function($event) {
-                                let idx = $event.rowIndex,
-                                    rowData = grid.getItem(idx);
-                                
-                                extMap.handleCenterLocation(rowData.latitude, rowData.longitude, null); 
-                            });
+                                // format the data for store/grid
+                                let dataStore = new ItemFileWriteStore({
+                                    data: payload
+                                });
+        
+                                // define the grid layout based on keys
+                                let layout = [[]];
+                                Object.keys(payload.items[0]).forEach(key => {
+                                    layout[0].push({ 'name': key, 'field': key, 'width': '100px' });
+                                });
+        
+                                let grid = new DataGrid({
+                                    id: 'grid_' + serviceObject.service.text,
+                                    store: dataStore,
+                                    structure: layout,
+                                    rowSelector: '20px'
+                                });
+        
+                                grid.on("RowClick", function($event) {
+                                    let idx = $event.rowIndex,
+                                        rowData = grid.getItem(idx);
+                                    
+                                    extMap.handleCenterLocation(rowData.latitude, rowData.longitude, null); 
+                                });
 
-                            self.tabs['grid_' + serviceObject.service.text] = grid;
-                            self.instance.addChild(cp3);
-    
-                            new Promise(function (resolve, reject) {
-                                grid.placeAt('div_' + serviceObject.service.text);
-                                grid.startup();
-                                resolve(grid);
-                            }).then(function (grid) {
-                                // grid.placeAt(cp3.containerNode);
-                                grid.height = "calc(250px - 69px)";
-                                grid.resize();
-                            });
+                                self.tabs['grid_' + serviceObject.service.text] = grid;
+                                self.instance.addChild(cp3);
+        
+                                new Promise(function (resolve, reject) {
+                                    grid.placeAt('div_' + serviceObject.service.text);
+                                    grid.startup();
+                                    resolve(grid);
+                                }).then(function (grid) {
+                                    // grid.placeAt(cp3.containerNode);
+                                    grid.height = "calc(250px - 69px)";
+                                    grid.resize();
+                                });
+                            }
                         }, function (error) {
                             console.log(error);
                         });
@@ -137,7 +143,7 @@ define(["dojo/_base/lang", "dijit/registry", "dojo/query",
 
                 let divContainer = self.tabs['div_' + serviceObject.service.text];
                 let gridContainer = self.tabs['grid_' + serviceObject.service.text];
-                let tabContainer = self.tabs['content_' + serviceObject.service.text];
+                let tabContainer = self.tabs['tab_' + serviceObject.service.text];
 
                 if (tabContainer && gridContainer) {
                     try {
@@ -171,7 +177,7 @@ define(["dojo/_base/lang", "dijit/registry", "dojo/query",
 
                 delete self.tabs['div_' + serviceObject.service.text];
                 delete self.tabs['grid_' + serviceObject.service.text];
-                delete self.tabs['content_' + serviceObject.service.text];
+                delete self.tabs['tab_' + serviceObject.service.text];
             };
 
             self.init();
